57f0c61091e74e4fabf04d1543d140402000f64e
Sukhy Dhami <s_r1992@hotmail.com>
2019-05-25 12:58:18 -0400

    adding front-end

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
+app.use(express.static("public"));
+
 app.get("/", function(req, res){
     res.send("WELCOME TO THE RESTAURANT");
 });
